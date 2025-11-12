6f447aa14bf91759b609bfa3736e8bbb4d566780
jicandina07 <jcandina@correo.unimet.edu.ve>
2025-11-12 16:05:35 -0400

    Crear clase Inventario vacía

---
+++ inventario.py
@@ -0,0 +1,16 @@
+class Inventario:
+
+    def __init__(self):
+        pass
+    
+    def visualizar(self):
+        pass
+
+    def verificar_stock(self, nombre_ingr):
+        pass
+
+    def mostrar_stock(self, categoria):
+        pass
+
+    def actualizar_stock(self, nombre_ingr):
+        pass
