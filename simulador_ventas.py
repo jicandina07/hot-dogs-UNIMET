bcb58c7e8372bc58f5ec858f1249ce33e067c31a
lucianodiciano <luciano.diciano@correo.unimet.edu.ve>
2025-11-14 19:06:55 -0400

    Corregir errores en SimuladorVentas

--- simulador_ventas.py
+++ simulador_ventas.py
@@ -7,6 +7,8 @@ class SimuladorVentas:
         self.menu = menu
         self.inventario = inventario
         self.gestor_ingredientes = gestor_ingredientes
+
+    def simular_dia(self):
         # Número aleatorio de clientes
         self.n_clientes = randint(0, 200)
         # Número de clientes que cambiaron de opinión
@@ -19,8 +21,6 @@ class SimuladorVentas:
         self.ingredientes_fallo = set()
         # Acompañantes vendidos
         self.acomp_vendidos = set()
-
-    def simular_dia(self):
         for i in range(self.n_clientes):
             # Número de hot dogs es aleatorio entre 0 y 5
             n_hotdogs = randint(0, 5)
@@ -37,23 +37,47 @@ class SimuladorVentas:
                 acomp_posibles = self.gestor_ingredientes.obtener_categoria("acompañante")
                 hotdog["Acompañante"] = [choice(acomp_posibles + [''])]
                 # Verificar disponibilidad de ingredientes para la orden
-                falta = self.inventario.verificar_stock(hotdog)
+                falta = None
+                for ingrediente in list(hotdog.values())[1:]:
+                  if not ingrediente:
+                      continue
+                  elif type(ingrediente) == list:
+                      for item in ingrediente:
+                          if not item:
+                              continue
+                          elif not self.inventario.revisar_stock(item):
+                              falta = item
+                  else:
+                      if not self.inventario.revisar_stock(ingrediente):
+                          falta = ingrediente
                 if falta:
                     print(f"El cliente {i} no pudo comprar {hotdog['nombre']} por falta de {falta}. Se marchó sin nada.")
                     self.n_fallos += 1
                     self.stats[hotdog["nombre"]]["fallos"] += 1
-                    self.ingredientes_fallo.add(hotdog["nombre"])
+                    self.ingredientes_fallo.add(falta)
                     break
                 orden_cliente.append(hotdog)
             else:
                 for hotdog_vendido in orden_cliente:
-                    self.inventario.actualizar_stock(hotdog_vendido)
+                    # Actualizar el stock de cada ingrediente
+                    for ingrediente in list(hotdog.values())[1:]:
+                        if not ingrediente:
+                            continue
+                        elif type(ingrediente) == list:
+                            for item in ingrediente:
+                                if not item:
+                                    continue
+                                self.inventario.restar_stock(item, cantidad=1)
+                        else:
+                            self.inventario.restar_stock(ingrediente, cantidad=1)
+                    # Actualizar el reporte de ventas
+                    self.stats[hotdog_vendido["nombre"]]["ventas"] += 1
                     if hotdog_vendido["Acompañante"]:
                         self.acomp_vendidos.update(hotdog_vendido["Acompañante"])
 
     def generar_reporte(self):
         ventas_totales = sum([hotdog["ventas"] for hotdog in self.stats.values()])
-        hotdog_mas_vendido = max(self.stats, key = lambda hotdog: self.stats[hotdog]["ventas"])
+        hotdog_mas_vendido = max(self.stats, key=lambda hotdog: self.stats[hotdog]["ventas"])
         print("")
         print("----------- Reporte de Ventas de UNIMET Hot Dogs -----------")
         print(f"Total de clientes: {self.n_clientes}")
@@ -61,7 +85,6 @@ class SimuladorVentas:
         print(f"Clientes que no pudieron comprar por falta de stock: {self.n_fallos}")
         print(f"Promedio de hot dogs por cliente: {ventas_totales / self.n_clientes}")
         print(f"Hot dog más vendido: {hotdog_mas_vendido}")
-        print(f"Hot dogs que no tuvieron stock: {[hotdog for hotdog in self.stats.values() if hotdog["fallos"] > 0]}")
+        print(f"Hot dogs que no tuvieron stock: {[hotdog for hotdog in self.stats if self.stats[hotdog]['fallos'] > 0]}")
         print(f"Ingredientes que no tuvieron stock: {self.ingredientes_fallo}")
         print(f"Acompañantes vendidos: {self.acomp_vendidos}")            
-
