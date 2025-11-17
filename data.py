916c333da14349bada011985b7dc92b40a62d81a
jicandina07 <jcandina@correo.unimet.edu.ve>
2025-11-16 20:45:14 -0400

    Convertir data en clase ManejadorData

--- data.py
+++ data.py
@@ -3,24 +3,27 @@ import os, json, requests
 
 URL = "https://raw.githubusercontent.com/FernandoSapient/BPTSP05_2526-1/main/"
 
-def cargar_datos_API():
-    try:
-        respuest_ingr = requests.get(URL + "ingredientes.json")
-        respuesta_menu = requests.get(URL + "menu.json")
-        respuest_ingr.raise_for_status()
-        respuesta_menu.raise_for_status()
-        ingredientes = respuest_ingr.json()
-        menu = respuesta_menu.json()
-    except Exception as e:
-        print(f"Error cargando ingredientes: {e}")
-        return None, None
-    return ingredientes, menu
 
+class ManejadorData:
 
-def cargar_archivo(archivo):
-    if not os.path.exists(archivo):
-        print(f"ERROR: el archivo {archivo} no existe. Por favor intente de nuevo.")
-        return None, None
-    with open(archivo) as f:
-        return json.load(f)
-    return None
+    def cargar_datos_API(self):
+        try:
+            respuest_ingr = requests.get(URL + "ingredientes.json")
+            respuesta_menu = requests.get(URL + "menu.json")
+            respuest_ingr.raise_for_status()
+            respuesta_menu.raise_for_status()
+            ingredientes = respuest_ingr.json()
+            menu = respuesta_menu.json()
+        except Exception as e:
+            print(f"Error cargando ingredientes: {e}")
+            return None, None
+        return ingredientes, menu
+    
+    
+    def cargar_archivo(self, archivo):
+        if not os.path.exists(archivo):
+            print(f"ERROR: el archivo {archivo} no existe. Por favor intente de nuevo.")
+            return None, None
+        with open(archivo) as f:
+            return json.load(f)
+        return None
