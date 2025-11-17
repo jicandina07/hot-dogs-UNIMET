5d2ecce295e36b910715868be9997c2529cff122
lucianodiciano <luciano.diciano@correo.unimet.edu.ve>
2025-11-16 22:01:32 -0400

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -32,20 +32,20 @@ def obtener_opciones_usuario(opciones):
     """
     res = []
     while True:
-        # Preguntar si quiere seguir añadiendo si ya lo ha hecho
+        
         if len(res) > 0:
             print("")
             print(f"¿Desea añadir más de estos items? (s/n)")
             if obtener_opcion_usuario(['s', 'n']) == 'n':
                 break
-        # Pedirle una opción de las opciones
+        
         opcion = input(f"Por favor ingrese una opción de {opciones}: ")
         if opcion not in opciones:
             print("")
             print(f"Opción inválida. Por favor ingrese uno de: {opciones}")
             print("")
             continue
-        # Advertir si ya se añadió previamente
+        
         elif opcion in res:
             print("")
             print(f"¡El item {opcion} ya fue añadido previamente!")
